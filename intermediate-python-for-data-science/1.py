d4482dce2ce9353c03170ece058cd6301bdc68a9
Reza Primasatya <rezaprimasatya@gmail.com>
2018-10-30 17:51:41 +0800

    intermediate

---
+++ intermediate-python-for-data-science/1.py
@@ -0,0 +1,13 @@
+# Print the last item from year and pop
+print(year)
+print(pop)
+
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+
+# Display the plot with plt.show()
+plt.show()
